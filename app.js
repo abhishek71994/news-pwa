e7638df96727258e9e3ea4d38023b4ebf876bf64
abhishek71994 <abhishek71994@gmail.com>
2018-02-27 10:15:50 +0530

    added sources, connected with api call

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const key = "2a57d3a0084c416bad1794a6e50fa675";
 const main = document.querySelector('main');
-window.addEventListener('load',e => {
+const select =  document.querySelector('#selector');
+const defaultSource = 'techcrunch';
+window.addEventListener('load',async e => {
 	updateNews();
+	await createSource();
+	select.value = defaultSource;
+
+	//adding a change event for the source selector
+	select.addEventListener('change', e => {
+		updateNews(e.target.value);
+	})
 })
-async function updateNews(){
+async function updateNews(source = defaultSource){
 	//update using the api
-	const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${key}`);
+	const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${key}`);
 	const json = await res.json();
 	main.innerHTML = json.articles.map(createArticle).join('\n');
 }
-
+async function createSource(){
+	const resSource = await fetch(`https://newsapi.org/v2/sources?apiKey=${key}`);
+	const jsonSource =  await resSource.json();
+	select.innerHTML = jsonSource.sources
+	.map(source => `<option value="${source.id}">${source.name}</option>`).join("\n");
+}
 function createArticle(article){
 	return `
 		<div class="article">
