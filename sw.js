d66ffa4d10544a243b54a79f9a8cb0de1b08c8b6
abhishek71994 <abhishek71994@gmail.com>
2018-03-01 02:43:00 +0530

    dynamic assets added resolves #6

--- sw.js
+++ sw.js
@@ -10,8 +10,24 @@ self.addEventListener('install',async event => {
 })
 self.addEventListener('fetch',event => {
 	const req = event.request;
-	event.respondWith(cacheFirst(req));
+	const url = new URL(req.url);
+	if(url.origin === location.origin){
+		event.respondWith(cacheFirst(req));
+	}else{
+		event.respondWith(networkFirst(req));
+	}
 })
+
+async function networkFirst(req){
+	const cache = await caches.open("news-dynamic-content");
+	try{
+		const res = await fetch(req);
+		cache.put(req,res.clone());
+		return res;
+	}catch(error){
+		return await cache.match(req);
+	}
+}
 
 async function cacheFirst(req){
 	const cachedResponse = await caches.match(req);
